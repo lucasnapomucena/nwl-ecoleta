51e1f802b4237b348463c45758341d86e73a0d37
Lucas Napomucena <lucasnapomucena@gmail.com>
2020-06-08 19:44:22 -0300

    Create App

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../src/App.css';
+import Routes from './routes';
+
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+export default App;
---
+++ web/src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+//Browser - Navegação do Browser
+import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/create-point" component={CreatePoint} />
+    </BrowserRouter>
+  )
+}
+
+export default Routes;
